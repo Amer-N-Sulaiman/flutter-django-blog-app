981cad4faf51638c3f2f1045f145571275d77a8b
Amer-N-Sulaiman <amersulaiman10@gmail.com>
2021-04-05 16:26:28 +0300

    Added Read More to the articles list page to read more of every article on a click of a button

--- app/lib/pages/articles.dart
+++ app/lib/pages/articles.dart
@@ -7,10 +7,15 @@ class Articles extends StatefulWidget {
   _ArticlesState createState() => _ArticlesState();
 }
 
-class _ArticlesState extends State<Articles> {
+class _ArticlesState extends State<Articles> with TickerProviderStateMixin{
+
+  
   
   @override
   Widget build(BuildContext context) {
+
+    List <AnimationController> controllers=[];
+    List <Animation> readMoreAnimations=[];
 
     ArticleProvider articlesP = Provider.of<ArticleProvider>(context);
 
@@ -32,24 +37,77 @@ class _ArticlesState extends State<Articles> {
     );
 
     for (int i=0; i<articlesP.articles.length; i++){
-      content.add(
-        ListTile(
-          title: Text(articlesP.articles[i].title),
-          subtitle: Text('${articlesP.articles[i].body.substring(0, 40)} ...'),
-          trailing: Container(
-            width: 40,
-            height: 40,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image.asset('images/${articlesP.articles[i].img}',
-              fit: BoxFit.cover)
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[300]),
-              color: Colors.white,
-            ),
-          ),
 
+      // The animation controllers
+      AnimationController controller = AnimationController(
+        duration: Duration(milliseconds: 100,),
+        vsync: this,
+      );
+
+      Animation _readMoreAnimation = TweenSequence(
+        <TweenSequenceItem<double>>[
+          TweenSequenceItem<double>(
+            tween: Tween(begin:50, end:400),
+            weight: 1,
+          ),
+          
+          
+        ]
+      ).animate(controller);
+
+      controllers.add(controller);
+      controllers[i].addListener(() {
+        print(readMoreAnimations[i].value);
+      });
+      readMoreAnimations.add(_readMoreAnimation);
+
+      String readMore = 'Read More!';
+      content.add(
+        AnimatedBuilder(
+          animation: controllers[i],
+          builder: (BuildContext context, _){
+            return ListTile(
+            title: Text(articlesP.articles[i].title),
+            subtitle: Row(children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width*0.6,
+                child: Text('${articlesP.articles[i].body.substring(0, (readMoreAnimations[i].value).round())} ...')
+              ),
+              TextButton(
+                child: Text('$readMore'),
+                onPressed: () {
+                  
+                  if (readMore == 'Read More!'){
+                    controllers[i].forward();
+                    readMore = 'Read Less!';
+                  } else {
+                    controllers[i].reverse();
+                    readMore = 'Read More!';
+                  }
+
+                  
+                },
+                )
+            ],),
+              
+            trailing: Container(
+              width: 40,
+              height: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.asset('images/${articlesP.articles[i].img}',
+                fit: BoxFit.cover)
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[300]),
+                color: Colors.white,
+              ),
+            ),
+
+          );
+          }
+
+          
         )
       );
     }
